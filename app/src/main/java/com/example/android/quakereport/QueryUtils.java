51693443aceb88beb7c882a28d9e4b2b8b3cbe31
ahmed <ahmed.mahmoud.el.beah@gmail.com>
2020-10-07 02:15:14 +0200

    FINALOK

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -103,6 +103,7 @@ public final class QueryUtils {
             }
 
         } catch (JSONException e) {
+
             // If an error is thrown when executing any of the above statements in the "try" block,
             // catch the exception here, so the app doesn't crash. Print a log message
             // with the message from the exception.
